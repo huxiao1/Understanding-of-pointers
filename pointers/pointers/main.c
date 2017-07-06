0c4dfe50295eaf6cf7b8e09e13d6f86102f871a5
huxaio <603316223@qq.com>
2017-07-06 09:39:19 +0800

    1

---
+++ pointers/pointers/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  pointers
+//
+//  Created by 胡啸 on 2017/7/6.
+//  Copyright © 2017年 胡啸. All rights reserved.
+//
+
+/*输出地址
+#include <stdio.h>
+
+int main(){
+    int a = 100;
+    char str[20] = "c.biancheng.net";
+    printf("%#X, %#X\n", &a, str);
+    return 0;
+}
+*/
+
+
